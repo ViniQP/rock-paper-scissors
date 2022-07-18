a3f8e263fe3fe687e503c7418858d83f8197957c
Vinícius M <viniqpoweryt@gmail.com>
2022-07-18 17:28:41 -0300

    Add UI to play rps rounds

--- script.js
+++ script.js
@@ -16,56 +16,108 @@ function computerPlay() {
    }
 }
 
+ function rpsRound() {
 
-
-function rpsRound() {
-    let playerSelection = prompt("Choose Rock, Paper or Scissors", "")
-    playerSelection = playerSelection.toUpperCase()
-    console.log(playerSelection)
     let = computerChoice = computerPlay()
-    console.log(computerChoice)
-
-    if(playerSelection === "ROCK") {
+    let choices = document.querySelector('.choices')
+    if (playerChoice === 'rock') {    
         if (computerChoice === "ROCK") {
+            choices.textContent = 'You chose Rock and the Computer chose Rock!'
             return "Draw!";
         }
         else if (computerChoice === "PAPER") {
-            return "You Lose! Paper beats Rock";
+            choices.textContent = 'You chose Rock and the Computer chose Paper!'
+            return "You Lose!";
         }
         else if (computerChoice === "SCISSORS") {
-            return "You Win! Rock beats Scissors";
+            choices.textContent = 'You chose Rock and the Computer chose Scissors!'
+            return "You Win!";
         }
     }
-    else if(playerSelection === "PAPER") {
+    if (playerChoice === 'paper') {
         if (computerChoice === "ROCK") {
-            return "You Win! Paper beats Rock";
+            choices.textContent = 'You chose Paper and the Computer chose Rock!'
+            return "You Win!";
         }
         else if (computerChoice === "PAPER") {
+            choices.textContent = 'You chose Paper and the Computer chose Paper!'
             return "Draw!";
         }
         else {
-            return "You Lose! Scissors beats Paper"
+            choices.textContent = 'You chose Paper and the Computer chose Scissors!'
+            return "You Lose!"
         }
     }
-    else if(playerSelection === "SCISSORS") {
+   
+    if (playerChoice === "scissors") {
         if (computerChoice === "ROCK") {
-            return "You Lose! Rock beats Scissors";
+            choices.textContent = 'You chose Scissors and the Computer chose Rock!'
+            return "You Lose!";
         }
         else if (computerChoice === "PAPER") {
-            return "You Win! Scissors beats Paper";
+            choices.textContent = 'You chose Scissors and the Computer chose Paper!'
+            return "You Win!";
         }
         else {
+            choices.textContent = 'You chose Scissors and the Computer chose Scissors!'
             return "Draw!"
         }
     }
-
-
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        console.log(rpsRound())
-    }
-}
-
-game()
+let playerChoice
+let playerScore = 0
+let computerScore = 0
+announcement = 0
+playerButtons = document.querySelectorAll('.player-choice')
+playerButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerChoice = button.id
+        result = rpsRound()
+        if (result === "You Win!") {
+            playerScore++
+        }
+        else if (result === "You Lose!") {
+            computerScore++
+        }
+        let content = document.querySelector('.results')
+        content.textContent = result
+        let score = document.querySelector('.score') 
+        let winner = document.createElement('p')  
+        if (playerScore < 5 && computerScore < 5) {       
+            score.textContent = `Player ${playerScore} x ${computerScore} Computer `
+        }
+        else if (playerScore > computerScore) {
+            if (announcement === 0) {
+                score.textContent = `Player ${playerScore} x ${computerScore} Computer `
+                winner.textContent = "Player Won!"
+                score.appendChild(winner)
+                const newGame = document.createElement('button')
+                newGame.classList.add('new-game')
+                newGame.textContent = 'Reload Page'
+                score.append(newGame)
+                newGame.addEventListener('click', () => {
+                    window.location.reload()
+                });
+                announcement++
+            }
+        }
+        else if (computerScore > playerScore) {
+            if (announcement === 0) {
+                score.textContent = `Player ${playerScore} x ${computerScore} Computer `
+                winner.textContent = "Computer Won!"
+                announcement++
+            
+                score.appendChild(winner)
+                const newGame = document.createElement('button')
+                newGame.classList.add('new-game')
+                newGame.textContent = 'Reload Page'
+                score.appendChild(newGame)
+                newGame.addEventListener('click', () => {
+                    window.location.reload()
+                })
+            }
+        }
+    
+    })
+})
